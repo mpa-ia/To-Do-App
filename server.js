7b680a821e211379145687a2a24112328af5f222
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-24 15:37:01 +0100

    Initialize websocket, emit updateData with tasks array on socket connection

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const socket = require('socket.io');
 
 const app = express();
 
 const server = app.listen(8000, () => {
     console.log('Server is running on port 8000');
+});
+
+const io = socket(server);
+
+const tasks = [];
+
+io.on('connection', (socket) => {
+    socket.emit('updateData', tasks);
 });
 
 app.use((req, res) => {
